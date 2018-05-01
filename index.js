d933d66558e239152639114b40cd58744a58c4c6
Max <max@colu.com>
2018-05-01 16:40:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const init = async function (osseus) {
+	const mongo = require(path.join(__dirname, '/lib/mongo'));
+	return mongo(osseus.config);
+}
+
+const start = async function () {
+
+};
+
+module.exports = {
+	init: init,
+	start: start
+};
