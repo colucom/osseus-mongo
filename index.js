8cbf055cd6628a3c845b09dbaf2e261f73b26252
Lior Rabin <lior@colu.com>
2018-07-23 17:44:45 +0300

    UnhandledPromiseRejectionWarning handling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const init = async (osseus) => {
   return new Promise(async (resolve, reject) => {
-    const mongo = await require(path.join(__dirname, '/lib/mongo'))(osseus.config)
+    const mongo = await require(path.join(__dirname, '/lib/mongo'))(osseus.config).catch(err => { reject(err) })
     resolve(mongo)
   })
 }
